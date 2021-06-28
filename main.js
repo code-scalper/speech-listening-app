1cde1334f0c9c10d87ff7fc8b911d5ff78f6d003
code-scalper <58999821+code-scalper@users.noreply.github.com>
2021-06-28 16:00:09 +0900

    listening

---
+++ main.js
@@ -0,0 +1,142 @@
+// DOM
+const checkButton = document.querySelector(".check-button");
+const saveButton = document.querySelector(".save-button");
+const startButton = document.querySelector("#start-button");
+const answerInput = document.querySelector(".answer-input");
+const wordInput = document.querySelector(".word-input");
+const stateText = document.querySelector(".state-text");
+const studyInfoTotal = document.querySelector(".total");
+const studyInfoCorrect = document.querySelector(".correct");
+const historyContainer = document.querySelector(".result");
+const historyList = document.querySelector(".result > ul");
+
+// Variables
+import { phrases, VOICE_TYPE } from "./phrases.js";
+const synth = window.speechSynthesis;
+let score = 0;
+let isPlaying = false;
+let voices = [];
+let gamePhrases = [];
+let currentPhrase = "";
+let voiceType = "native";
+
+// functions
+function init() {
+  stateText.innerText = `Ready with ${phrases.length} phrases`;
+  gamePhrases = [...phrases];
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoiceList;
+  }
+}
+
+function playGame() {
+  if (gamePhrases.length === 0) {
+    gameOver();
+    return;
+  }
+  currentPhrase = gamePhrases[0];
+  gamePhrases.shift();
+  speak(currentPhrase.eng);
+}
+
+function setVoices(type) {
+  return voices.filter((sound) => VOICE_TYPE[type].includes(sound.lang));
+}
+
+function populateVoiceList() {
+  voices = synth.getVoices().sort(function (a, b) {
+    const aname = a.name.toUpperCase(),
+      bname = b.name.toUpperCase();
+    return aname < bname ? 1 : -1;
+  });
+  voices = setVoices(voiceType);
+}
+
+function genRandomVoiceIndex() {
+  const index = Math.floor(Math.random() * voices.length);
+  return index;
+}
+
+function speak(text) {
+  const selectedVoice = voices[genRandomVoiceIndex()];
+
+  if (synth.speaking) {
+    console.error("speechSynthesis.speaking");
+    return;
+  }
+  if (text !== "") {
+    const utterThis = new SpeechSynthesisUtterance(text);
+    utterThis.onend = function (event) {
+      answerInput.focus();
+    };
+    utterThis.onerror = function (event) {
+      console.error("SpeechSynthesisUtterance.onerror");
+    };
+    utterThis.voice = selectedVoice;
+    utterThis.pitch = 1;
+    utterThis.rate = 1;
+    console.log(synth, utterThis, "utterThis");
+    synth.speak(utterThis);
+  }
+}
+
+function setIcon(oldClass, newClass, order = null, additionalClass = null) {
+  const target = document.querySelector(`.${oldClass}`);
+  if (!target) return;
+  target.classList.add(newClass);
+  target.classList.remove(oldClass);
+  if (order) {
+    target.classList[order](additionalClass);
+  }
+}
+function checkAnswer() {
+  const currWord = currentPhrase.eng;
+  const ansWord = answerInput.value;
+  const currWordUpper = currWord.toUpperCase();
+  const ansWordUpper = ansWord.toUpperCase();
+  let i = 0;
+  const targetLength =
+    currWordUpper.length > ansWordUpper.length
+      ? currWordUpper.length
+      : ansWordUpper.length;
+  let aSpan = currentPhrase.eng;
+  let bSpan = "";
+  while (i < targetLength) {
+    let letter = currWord.charAt(i);
+    if (currWordUpper.charAt(i) !== ansWordUpper.charAt(i)) {
+      letter = `<span class='error'>${ansWord.charAt(i)}</span>`;
+    }
+    bSpan = bSpan + letter;
+    i++;
+  }
+  const contents = `<span>${aSpan}</span><span>${bSpan}</span>`;
+  const li = document.createElement("li");
+  li.innerHTML = contents;
+  historyList.appendChild(li);
+  historyContainer.scroll({
+    top: historyContainer.scrollHeight,
+    behavior: "smooth",
+  });
+  answerInput.value = "";
+  playGame();
+}
+// event listeners
+checkButton.addEventListener("click", () => checkAnswer());
+answerInput.addEventListener("keypress", (e) => {
+  if (e.keyCode !== 13) return;
+  checkAnswer();
+});
+
+startButton.addEventListener("click", () => {
+  if (isPlaying) {
+    setIcon("bi-pause-fill", "bi-play-fill", "remove", "error");
+    isPlaying = false;
+  } else {
+    setIcon("bi-play-fill", "bi-pause-fill", "add", "error");
+    isPlaying = true;
+    playGame();
+  }
+});
+
+// initial call
+init();
---
+++ phrases.js
@@ -0,0 +1,42 @@
+export const phrases = [
+  { eng: "What have you been up to lately?", kor: "요즘 뭐해?" },
+  { eng: "How’s it going?", kor: "어떻게 지내?" },
+  { eng: "I really appreciate it.", kor: "정말 고마워" },
+  { eng: "That’s so kind of you.", kor: "정말 친절하구나" },
+  { eng: "I owe you one. ", kor: "내가 신세졌네" },
+  { eng: "It was nice chatting with you.", kor: "대화하니까 좋다" },
+  { eng: "Anyway, I should get going.", kor: "어쨌건 나는 가야겠어" },
+  { eng: "I’ve never given it much thought.", kor: "어짜피 많이 준적은 없다" },
+  { eng: "I don’t have strong feelings either way.", kor: "음 둘다 좀 그렇네" },
+  {
+    eng: "It doesn’t make any difference to me.",
+    kor: "나는 사실 아무 차이없어",
+  },
+  {
+    eng: "I have no opinion on the matter.",
+    kor: "그 문제에 대해 별 생각이 없네",
+  },
+  { eng: "I’m exhausted.", kor: "엄청 피곤하네" },
+  {
+    eng: "I can hardly keep my eyes open",
+    kor: "졸려서 눈 뜨고 있기가 힘드네",
+  },
+  { eng: "I’m gonna hit the sack.", kor: "자러가야 겠다" },
+];
+
+export const VOICE_TYPE = {
+  native: ["en-GB", "en-US"],
+  esl: [
+    "ja-JP",
+    "ko-KR",
+    "pl-PL",
+    "de-DE",
+    "en-ES",
+    "fr-FR",
+    "hi-IN",
+    "ru-RU",
+    "zh-CN",
+    "zh-HK",
+    "zh-TW",
+  ],
+};
